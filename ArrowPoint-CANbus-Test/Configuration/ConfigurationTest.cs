eadce30fb628da938e35faecf9eb0a7cd3e034a4
Cameron Tuesley <ctuesley@integraltech.com.au>
2019-06-26 23:02:28 +1000

    Basic implementation of a KCD based configuration loader

---
+++ ArrowPoint-CANbus-Test/Configuration/ConfigurationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ArrowPointCANBusTool.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrowPointCANBusTest.Configuration
+{
+    [TestClass]
+    public class ConfigurationTest
+    {
+
+        private static string GetTestDataFolder(string testDataFolder)
+        {
+            string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+            var pathItems = startupPath.Split(Path.DirectorySeparatorChar);
+            var pos = pathItems.Reverse().ToList().FindIndex(x => string.Equals("bin", x));
+            string projectPath = String.Join(Path.DirectorySeparatorChar.ToString(), pathItems.Take(pathItems.Length - pos - 1));
+            return Path.Combine(projectPath, "Test-Data", testDataFolder);
+        }
+
+        [TestMethod]
+        public void TestLoad()
+        {
+            string path = GetTestDataFolder("Configuration");
+
+            ConfigManager configManager = new ConfigManager();
+            configManager.LoadConfig(path + "\\CanConfig.xml");
+        }
+    }
+}
