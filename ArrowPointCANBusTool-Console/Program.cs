d7290a763755af92f0b6e5349b62319349571c2e
Cameron Tuesley <cameron@prohelion.com>
2020-03-02 19:08:51 +1000

    Totally broken build, switching PCs

---
+++ ArrowPointCANBusTool-Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using ArrowPointCANBusTool.Services;
+using System;
+
+namespace ArrowPointCANBusTool_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CanRecordReplayDebugService rbs = CanRecordReplayDebugService.NewInstance;
+        }
+    }
+}
--- ArrowPointCanBusToolLibrary/Services/CanRecordReplayDebugService.cs
+++ ArrowPointCanBusToolLibrary/Services/CanRecordReplayDebugService.cs
@@ -11,7 +11,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ArrowPointCANBusTool.Services
 {
